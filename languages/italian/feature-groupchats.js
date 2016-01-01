f0d2b918aeb928305bf080557412980f4d2b3262
Lake456 <rudmocazz@gmail.com>
2016-01-01 20:48:52 +0100

    Update feature-groupchats.js

--- languages/italian/feature-groupchats.js
+++ languages/italian/feature-groupchats.js
@@ -2,34 +2,34 @@
 	commands: {
 		autopromote: {
 			'notchat': 'Questo comando è disponibile solo per le chat room',
-			'on': 'Caratteristica promozione automatica è stata attivata per questa room',
-			'off': 'Caratteristica promozione automatica è stata disabilitata per questa room',
+			'on': 'La promozione automatica è stata attivata per questa room.',
+			'off': 'La promozione automatica è stata disabilitata per questa room',
 			'usage': 'Usage',
-			'dis': 'Promozione automatica è disabilitata per questa room',
+			'dis': 'La Promozione automatica è disabilitata per questa room',
 			'list': 'Promozione automatica in',
 			'list2': 'Promozione generale',
-			'err': 'Errore: non è riuscito a caricare lista autopromozione a hastebin',
+			'err': 'Errore: non sono riuscito a caricare la lista di autopromote su hastebin',
 			'or': 'o',
 			'usage2': 'per tutti gli utenti',
-			'general1': 'Autopromozione generale è stato disattivato',
-			'general2': 'Autopromozione generale è stata attivata per il rango',
+			'general1': 'L\'Autopromozione generale è stata disattivata',
+			'general2': 'L\'Autopromozione generale è stata attivata per il rango',
 			'rank': 'Rango',
 			'notrank': 'non trovato',
 			'user': 'Utente',
 			'notuser': 'non è nella lista di autopromozione',
-			'del': 'è stato eliminato dalla lista autopromozione',
+			'del': 'è stato eliminato dalla lista di autopromozione',
 			'auser': 'Autopromozione per l\'utente',
 			'set': 'era impostato su'
 		},
 		wpm: {
 			'notchat': 'Questo comando è disponibile solo per le chat room',
 			'usage': 'Usage',
-			'on': 'Privato caratteristica messaggio di benvenuto è stata attivata per room',
-			'off': 'Privato caratteristica messaggio di benvenuto è stato disattivato per room',
-			'dis': 'Privato caratteristica messaggio di benvenuto è disabilitata per room',
-			'set': 'Messaggio di benvenuto privato è stato modificato per room',
-			'not': 'Non c\'è nessun messaggio di benvenuto privato per room',
-			'del': 'Messaggio di benvenuto privato è stato rimosso per room'
+			'on': 'Il messaggio di benvenuto è stata attivato per questa room',
+			'off': 'Il messaggio di benvenuto è stato disattivato per questa room',
+			'dis': 'Il messaggio di benvenuto è stato disabilitato per questa room',
+			'set': 'Il Messaggio di benvenuto privato è stato modificato per questa room',
+			'not': 'Non c\'è nessun messaggio di benvenuto privato per questa room',
+			'del': 'Il Messaggio di benvenuto privato è stato rimosso per questa room'
 		}
 	}
 };
